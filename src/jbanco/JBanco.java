81cf4154c0d0d016ba821a092fb7112068bbd696
LOUISVUITTON-code <lulubi888@gmail.com>
2023-06-09 12:13:22 +0200

    primer commit luis

---
+++ src/jbanco/JBanco.java
@@ -0,0 +1,17 @@
+
+package jbanco;
+
+//@author LOUISVUITTON
+
+public class JBanco {
+    
+    static int saldo = 100;
+    
+    public static void main(String[] args){
+        System.out.println("Hola soy Luis y este es mi saldo"+ saldo);
+    }
+    
+    public static void saludo(){
+        System.out.println("HOLA JONATHAN SOY EL LUIS");
+    }
+}
